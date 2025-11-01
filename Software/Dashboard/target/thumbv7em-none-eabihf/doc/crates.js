99a26a3bf1e98b0cc5451362cb6328188d20b464
DashCampbell <dash9campbell@gmail.com>
2025-11-01 13:25:51 -0600

    Fix missing files

---
+++ Software/Dashboard/target/thumbv7em-none-eabihf/doc/crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["Dashboard"];
+//{"start":21,"fragment_lengths":[11]}
---
+++ Software/Dashboard/target/thumbv7em-none-eabihf/doc/src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["Dashboard",["",[],["display.rs","eco_can.rs","led.rs","lib.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[67]}
